e395d2a5f9140ee76e4ec841588071a102ccad1b
Nikita <zitros.lab@gmail.com>
2018-01-06 18:41:42 +0200

    [Fix] Server/client little fixes

--- src/global/const.js
+++ src/global/const.js
@@ -1,5 +1,5 @@
 module.exports = {
-	web3ProviderHost: location.hostname,
+	web3ProviderHost: typeof location !== "undefined" ? location.hostname : "127.0.0.1",
 	web3ProviderPort: 9545,
 	graphQlEndpoint: "/graphql",
 	graphQlPort: 4000
