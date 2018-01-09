ff9e2fcfb59e55167780736a15106afee9c265f6
ZitRos <zitros.lab@gmail.com>
2018-01-09 21:45:44 +0200

    [Fix] Network

--- src/ethereum/truffle.js
+++ src/ethereum/truffle.js
@@ -1,9 +1,4 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-	networks: {
-		development: {
-			gas: 4600000
-		}
-	}
 };
